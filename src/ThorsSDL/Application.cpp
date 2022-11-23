534a9245048f422484a4c8d47dde012db35f7f17
Loki Astari <Loki.Astari@gmail.com>
2022-11-22 22:05:20 -0800

    Add handlers for all known events

--- src/ThorsSDL/Application.cpp
+++ src/ThorsSDL/Application.cpp
@@ -82,11 +82,110 @@ void Application::handleEvents()
     {
         switch (event.type)
         {
-            case SDL_QUIT:  exitLoop();break;
-            default:
-                break;
+    /* Application events       0x010*  */
+            case SDL_QUIT:                      return handleEventQuit(event.quit);                 // SDL_QuitEvent
+            case SDL_APP_TERMINATING:           return handleEventTerminating(event.common);        // SDL_CommonEvent
+            case SDL_APP_LOWMEMORY:             return handleEventLowMemory(event.common);          // SDL_CommonEvent
+            case SDL_APP_WILLENTERBACKGROUND:   return handleEventBackground(event.common, false);  // SDL_CommonEvent
+            case SDL_APP_DIDENTERBACKGROUND:    return handleEventBackground(event.common, true);   // SDL_CommonEvent
+            case SDL_APP_WILLENTERFOREGROUND:   return handleEventForeground(event.common, false);  // SDL_CommonEvent
+            case SDL_APP_DIDENTERFOREGROUND:    return handleEventForeground(event.common, true);   // SDL_CommonEvent
+            case SDL_LOCALECHANGED:             return handleEventLocalUpdate(event.common);        // SDL_CommonEvent
+
+    /* Display events           0x015*  */
+            case SDL_DISPLAYEVENT:              return handleEventDisplayEvent(event.display);      // SDL_DisplayEvent
+
+    /* Window events            0x020*  */
+            case SDL_WINDOWEVENT:               return handleEventWindowEvent(event.window);        // SDL_WindowEvent
+            case SDL_SYSWMEVENT:                return handleEventSystemEvent(event.syswm);         // SDL_SysWMEvent
+
+    /* Keyboard events          0x030*  */
+            case SDL_KEYDOWN:                   return handleEventKeyDown(event.key);               // SDL_KeyboardEvent
+            case SDL_KEYUP:                     return handleEventKeyUp(event.key);                 // SDL_KeyboardEvent
+            case SDL_TEXTEDITING:               return handleEventTextEditting(event.edit);         // SDL_TextEditingEvent
+            case SDL_TEXTINPUT:                 return handleEventTextInput(event.text);            // SDL_TextInputEvent
+            case SDL_KEYMAPCHANGED:             return handleEventKeyMapChanged(event.common);      // SDL_CommonEvent
+            case SDL_TEXTEDITING_EXT:           return handleEventTextEditingExt(event.editExt);    // SDL_TextEditingExtEvent
+
+    /* Mouse events             0x040*  */
+            case SDL_MOUSEMOTION:               return handleEventMouseMove(event.motion);          // SDL_MouseMotionEvent
+            case SDL_MOUSEBUTTONDOWN:           return handleEventMouseDown(event.button);          // SDL_MouseButtonEvent
+            case SDL_MOUSEBUTTONUP:             return handleEventMouseUp(event.button);            // SDL_MouseButtonEvent
+            case SDL_MOUSEWHEEL:                return handleEventMouseWheel(event.wheel);          // SDL_MouseWheelEvent
+
+    /* Joystick events          0x060*  */
+            case SDL_JOYAXISMOTION:             return handleEventJoyMotion(event.jaxis);           // SDL_JoyAxisEvent
+            case SDL_JOYBALLMOTION:             return handleEventJoyBall(event.jball);             // SDL_JoyBallEvent
+            case SDL_JOYHATMOTION:              return handleEventJoyHat(event.jhat);               // SDL_JoyHatEvent
+            case SDL_JOYBUTTONDOWN:             return handleEventJoyButtonDown(event.jbutton);     // SDL_JoyButtonEvent
+            case SDL_JOYBUTTONUP:               return handleEventJoyButtonUp(event.jbutton);       // SDL_JoyButtonEvent
+            case SDL_JOYDEVICEADDED:            return handleEventJoyDeviceAdd(event.jdevice);      // SDL_JoyDeviceEvent
+            case SDL_JOYDEVICEREMOVED:          return handleEventJoyDeviceRem(event.jdevice);      // SDL_JoyDeviceEvent
+            case SDL_JOYBATTERYUPDATED:         return handleEventJoyBattery(event.jbattery);       // SDL_JoyBatteryEvent
+
+    /* Game controller events   0x065*  */
+            case SDL_CONTROLLERAXISMOTION:      return handleEventControlMotion(event.caxis);       // SDL_ControllerAxisEvent
+            case SDL_CONTROLLERBUTTONDOWN:      return handleEventControlButtonDown(event.cbutton); // SDL_ControllerButtonEvent
+            case SDL_CONTROLLERBUTTONUP:        return handleEventControlButtonUp(event.cbutton);   // SDL_ControllerButtonEvent
+            case SDL_CONTROLLERDEVICEADDED:     return handleEventControlDeviceAdd(event.cdevice);  // SDL_ControllerDeviceEvent
+            case SDL_CONTROLLERDEVICEREMOVED:   return handleEventControlDeviceRem(event.cdevice);  // SDL_ControllerDeviceEvent
+            case SDL_CONTROLLERDEVICEREMAPPED:  return handleEventControlDeviceMap(event.cdevice);  // SDL_ControllerDeviceEvent
+
+            case SDL_CONTROLLERTOUCHPADDOWN:    return handleEventTouchPadDown(event.ctouchpad);    // SDL_ControllerTouchpadEvent
+            case SDL_CONTROLLERTOUCHPADUP:      return handleEventTouchPadUp(event.ctouchpad);      // SDL_ControllerTouchpadEvent
+            case SDL_CONTROLLERTOUCHPADMOTION:  return handleEventTouchPadMotion(event.ctouchpad);  // SDL_ControllerTouchpadEvent
+            case SDL_CONTROLLERSENSORUPDATE:    return handleEventTouchPadSendor(event.csensor);    // SDL_ControllerSensorEvent
+
+    /* Touch events             0x070*  */
+            case SDL_FINGERDOWN:                return handleEventFingerDown(event.tfinger);        // SDL_TouchFingerEvent
+            case SDL_FINGERUP:                  return handleEventFingerUp(event.tfinger);          // SDL_TouchFingerEvent
+            case SDL_FINGERMOTION:              return handleEventFingerMotion(event.tfinger);      // SDL_TouchFingerEvent
+
+    /* Gesture events           0x080*  */
+            case SDL_DOLLARGESTURE:             return handleEventDollarGesture(event.dgesture);    // SDL_DollarGestureEvent
+            case SDL_DOLLARRECORD:              return handleEventDollarRecord(event.dgesture);     // SDL_DollarGestureEvent
+            case SDL_MULTIGESTURE:              return handleEventMultiGesture(event.mgesture);     // SDL_MultiGestureEvent
+
+    /* Clipboard events         0x09*   */
+            case SDL_CLIPBOARDUPDATE:           return handleEventClipboardUpdate(event.common);    // SDL_CommonEvent
+
+    /* Drag and drop events     0x100*  */
+            case SDL_DROPFILE:                  return handleEventDropFile(event.drop);             // SDL_DropEvent
+            case SDL_DROPTEXT:                  return handleEventDropText(event.drop);             // SDL_DropEvent
+            case SDL_DROPBEGIN:                 return handleEventDropBegin(event.drop);            // SDL_DropEvent
+            case SDL_DROPCOMPLETE:              return handleEventDropEnd(event.drop);              // SDL_DropEvent
+
+    /* Audio hotplug events     0x110*  */
+            case SDL_AUDIODEVICEADDED:          return handleEventAudioDeviceAdd(event.adevice);    // SDL_AudioDeviceEvent
+            case SDL_AUDIODEVICEREMOVED:        return handleEventAudioDeviceRem(event.adevice);    // SDL_AudioDeviceEvent
+
+    /* Sensor events            0x120*  */
+            case SDL_SENSORUPDATE:              return handleEventSensorUpdate(event.sensor);       // SDL_SensorEvent
+
+    /* Render events            0x200*  */
+            case SDL_RENDER_TARGETS_RESET:      return handleEventRenderTargetReset(event.common);  // SDL_CommonEvent
+            case SDL_RENDER_DEVICE_RESET:       return handleEventRenderDeviceReset(event.common);  // SDL_CommonEvent
+
+    /* Internal events          0x7F0*  */
+            case SDL_POLLSENTINEL:              return handleEventPollSentinel(event.common);       // SDL_CommonEvent
+
+    /* User events              0x800*  */
+            case SDL_USEREVENT:                 return handleEventUser(event.user);                 // SDL_UserEvent
+
+            default:                            return handleEventUnknown(event.common);            // SDL_CommonEvent
         }
     }
+}
+
+void Application::handleEventQuit(SDL_QuitEvent const& /*event*/)
+{
+    exitLoop();
+}
+
+void Application::handleEventUnknown(SDL_CommonEvent const& event)
+{
+    std::cerr << "Unknown Event: " << event.type << "\n";
+    throw std::runtime_error("Unknown Event");
 }
 
 void Application::drawWindows()
--- src/ThorsSDL/Application.h
+++ src/ThorsSDL/Application.h
@@ -62,6 +62,97 @@ class Application
         void unregisterWindow(Window& window);
 
     private:
+        virtual void handleEventQuit(SDL_QuitEvent const& /*event*/);
+        virtual void handleEventTerminating(SDL_CommonEvent const& /*event*/)                   {}
+        virtual void handleEventLowMemory(SDL_CommonEvent const& /*event*/)                     {}
+        virtual void handleEventBackground(SDL_CommonEvent const& /*event*/, bool /*done*/)     {}
+        virtual void handleEventForeground(SDL_CommonEvent const& /*event*/, bool /*done*/)     {}
+        virtual void handleEventLocalUpdate(SDL_CommonEvent const& /*event*/)                   {}
+
+    /* Display events           0x015*  */
+        virtual void handleEventDisplayEvent(SDL_DisplayEvent const& /*event*/)                 {}
+
+    /* Window events            0x020*  */
+        virtual void handleEventWindowEvent(SDL_WindowEvent const& /*event*/)                   {}
+        virtual void handleEventSystemEvent(SDL_SysWMEvent const& /*event*/)                    {}
+
+    /* Keyboard events          0x030*  */
+        virtual void handleEventKeyDown(SDL_KeyboardEvent const& /*event*/)                     {}
+        virtual void handleEventKeyUp(SDL_KeyboardEvent const& /*event*/)                       {}
+        virtual void handleEventTextEditting(SDL_TextEditingEvent const& /*event*/)             {}
+        virtual void handleEventTextInput(SDL_TextInputEvent const& /*event*/)                  {}
+        virtual void handleEventTextEditingExt(SDL_TextEditingExtEvent const& /*event*/)        {}
+        virtual void handleEventKeyMapChanged(SDL_CommonEvent const& /*event*/)                 {}
+
+    /* Mouse events             0x040*  */
+        virtual void handleEventMouseMove(SDL_MouseMotionEvent const& /*event*/)                {}
+        virtual void handleEventMouseDown(SDL_MouseButtonEvent const& /*event*/)                {}
+        virtual void handleEventMouseUp(SDL_MouseButtonEvent const& /*event*/)                  {}
+        virtual void handleEventMouseWheel(SDL_MouseWheelEvent const& /*event*/)                {}
+
+    /* Joystick events          0x060*  */
+        virtual void handleEventJoyMotion(SDL_JoyAxisEvent const& /*event*/)                    {}
+        virtual void handleEventJoyBall(SDL_JoyBallEvent const& /*event*/)                      {}
+        virtual void handleEventJoyHat(SDL_JoyHatEvent const& /*event*/)                        {}
+        virtual void handleEventJoyButtonDown(SDL_JoyButtonEvent const& /*event*/)              {}
+        virtual void handleEventJoyButtonUp(SDL_JoyButtonEvent const& /*event*/)                {}
+        virtual void handleEventJoyDeviceAdd(SDL_JoyDeviceEvent const& /*event*/)               {}
+        virtual void handleEventJoyDeviceRem(SDL_JoyDeviceEvent const& /*event*/)               {}
+        virtual void handleEventJoyBattery(SDL_JoyBatteryEvent const& /*event*/)                {}
+
+    /* Game controller events   0x065*  */
+        virtual void handleEventControlMotion(SDL_ControllerAxisEvent const& /*event*/)         {}
+        virtual void handleEventControlButtonDown(SDL_ControllerButtonEvent const& /*event*/)   {}
+        virtual void handleEventControlButtonUp(SDL_ControllerButtonEvent const& /*event*/)     {}
+        virtual void handleEventControlDeviceAdd(SDL_ControllerDeviceEvent const& /*event*/)    {}
+        virtual void handleEventControlDeviceRem(SDL_ControllerDeviceEvent const& /*event*/)    {}
+        virtual void handleEventControlDeviceMap(SDL_ControllerDeviceEvent const& /*event*/)    {}
+
+        virtual void handleEventTouchPadDown(SDL_ControllerTouchpadEvent const& /*event*/)      {}
+        virtual void handleEventTouchPadUp(SDL_ControllerTouchpadEvent const& /*event*/)        {}
+        virtual void handleEventTouchPadMotion(SDL_ControllerTouchpadEvent const& /*event*/)    {}
+        virtual void handleEventTouchPadSendor(SDL_ControllerSensorEvent const& /*event*/)      {}
+
+    /* Touch events             0x070*  */
+        virtual void handleEventFingerDown(SDL_TouchFingerEvent const& /*event*/)               {}
+        virtual void handleEventFingerUp(SDL_TouchFingerEvent const& /*event*/)                 {}
+        virtual void handleEventFingerMotion(SDL_TouchFingerEvent const& /*event*/)             {}
+
+    /* Gesture events           0x080*  */
+        virtual void handleEventDollarGesture(SDL_DollarGestureEvent const& /*event*/)          {}
+        virtual void handleEventDollarRecord(SDL_DollarGestureEvent const& /*event*/)           {}
+        virtual void handleEventMultiGesture(SDL_MultiGestureEvent const& /*event*/)            {}
+
+    /* Clipboard events         0x09*   */
+        virtual void handleEventClipboardUpdate(SDL_CommonEvent const& /*event*/)               {}
+
+    /* Drag and drop events     0x100*  */
+        virtual void handleEventDropFile(SDL_DropEvent const& /*event*/)                        {}
+        virtual void handleEventDropText(SDL_DropEvent const& /*event*/)                        {}
+        virtual void handleEventDropBegin(SDL_DropEvent const& /*event*/)                       {}
+        virtual void handleEventDropEnd(SDL_DropEvent const& /*event*/)                         {}
+
+    /* Audio hotplug events     0x110*  */
+        virtual void handleEventAudioDeviceAdd(SDL_AudioDeviceEvent const& /*event*/)           {}
+        virtual void handleEventAudioDeviceRem(SDL_AudioDeviceEvent const& /*event*/)           {}
+
+    /* Sensor events            0x120*  */
+        virtual void handleEventSensorUpdate(SDL_SensorEvent const& /*event*/)                  {}
+
+    /* Render events            0x200*  */
+        virtual void handleEventRenderTargetReset(SDL_CommonEvent const& /*event*/)             {}
+        virtual void handleEventRenderDeviceReset(SDL_CommonEvent const& /*event*/)             {}
+
+    /* Internal events          0x7F0*  */
+        virtual void handleEventPollSentinel(SDL_CommonEvent const& /*event*/)                  {}
+
+    /* User events              0x800*  */
+        virtual void handleEventUser(SDL_UserEvent const& /*event*/)                            {}
+
+
+        virtual void handleEventUnknown(SDL_CommonEvent const& /*event*/);
+
+    private:
         void handleEvents();
         void drawWindows();
 };
