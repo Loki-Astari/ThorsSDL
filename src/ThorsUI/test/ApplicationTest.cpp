16a38dd9f467001227b18f39d212d46fa4eea4b4
Loki Astari <Loki.Astari@gmail.com>
2023-01-07 13:56:29 -0800

    Add Application UnitTests

--- src/ThorsUI/test/ApplicationTest.cpp
+++ src/ThorsUI/test/ApplicationTest.cpp
@@ -41,6 +41,29 @@ TEST(ApplicationTest, CreateAnApplicationInitFont)
     EXPECT_EQ(1, actions.count[countSDL_Quit]);
     EXPECT_EQ(1, actions.count[countTTF_Init]);
     EXPECT_EQ(1, actions.count[countTTF_Quit]);
+    EXPECT_EQ(0, actions.count[countIMG_Init]);
+    EXPECT_EQ(0, actions.count[countIMG_Quit]);
+}
+
+TEST(ApplicationTest, CreateAnApplicationInitImages)
+{
+    MocksSDLActions     actions;
+    MockSDL             mockActivate(actions);
+
+    auto action = []()
+    {
+        ThorsAnvil::UI::Application     application(ThorsAnvil::UI::Images);
+    };
+
+    EXPECT_NO_THROW(
+        action()
+    );
+    EXPECT_EQ(1, actions.count[countSDL_Init]);
+    EXPECT_EQ(1, actions.count[countSDL_Quit]);
+    EXPECT_EQ(0, actions.count[countTTF_Init]);
+    EXPECT_EQ(0, actions.count[countTTF_Quit]);
+    EXPECT_EQ(1, actions.count[countIMG_Init]);
+    EXPECT_EQ(1, actions.count[countIMG_Quit]);
 }
 
 TEST(ApplicationTest, CreateTwoApplication)
@@ -419,5 +442,124 @@ TEST(ApplicationTest, ApplicationWithWindowThatNeedsDrawing)
     EXPECT_NO_THROW(
         action()
     );
+}
+
+TEST(ApplicationTest, ApplicationGetHardware)
+{
+    MocksSDLActions     actions;
+    MockSDL             mockActivate(actions);
+
+    int     count;
+    Uint8*  keys = actions.mockSDL_GetKeyboardState(&count);
+    ASSERT_EQ(0, actions.count[countSDL_GetKeyboardState]);
+    ASSERT_NE(nullptr, keys);
+    ASSERT_TRUE(SDL_SCANCODE_Q < count);
+    keys[SDL_SCANCODE_Q] = 1;
+
+    bool qPressed = false;
+    bool wPressed = false;
+    auto action = [&qPressed, &wPressed]()
+    {
+        ThorsAnvil::UI::Application             application;
+        ThorsAnvil::UI::Hardware const&         hardware = application.getHardwareInfo();
+        ThorsAnvil::UI::KeyboardState const&    keyboard = hardware.getKeyboardState();
+
+        qPressed = keyboard.isPressed(SDL_SCANCODE_Q);
+        wPressed = keyboard.isPressed(SDL_SCANCODE_W);
+    };
+
+    EXPECT_NO_THROW(
+        action();
+    );
+
+    ASSERT_EQ(1, actions.count[countSDL_GetKeyboardState]);
+    ASSERT_TRUE(qPressed);
+    ASSERT_FALSE(wPressed);
+}
+
+TEST(ApplicationTest, ApplicationStart1Stop0Text)
+{
+    MocksSDLActions     actions;
+    MockSDL             mockActivate(actions);
+    ThorsAnvil::UI::Application             application;
+
+    auto action = [&application]()
+    {
+        application.enableTextInput(true);
+    };
+
+    EXPECT_NO_THROW(
+        action();
+    );
+
+    ASSERT_EQ(1, actions.count[countSDL_StartTextInput]);
+    ASSERT_EQ(0, actions.count[countSDL_StopTextInput]);
+    application.enableTextInput(false);
+}
+
+TEST(ApplicationTest, ApplicationStart2Stop0Text)
+{
+    MocksSDLActions     actions;
+    MockSDL             mockActivate(actions);
+    ThorsAnvil::UI::Application             application;
+
+    auto action = [&application]()
+    {
+        application.enableTextInput(true);
+        application.enableTextInput(true);
+    };
+
+    EXPECT_NO_THROW(
+        action();
+    );
+
+    ASSERT_EQ(1, actions.count[countSDL_StartTextInput]);
+    ASSERT_EQ(0, actions.count[countSDL_StopTextInput]);
+    application.enableTextInput(false);
+    application.enableTextInput(false);
+}
+
+TEST(ApplicationTest, ApplicationStart2Stop1Text)
+{
+    MocksSDLActions     actions;
+    MockSDL             mockActivate(actions);
+    ThorsAnvil::UI::Application             application;
+
+    auto action = [&application]()
+    {
+        application.enableTextInput(true);
+        application.enableTextInput(true);
+        application.enableTextInput(false);
+    };
+
+    EXPECT_NO_THROW(
+        action();
+    );
+
+    ASSERT_EQ(1, actions.count[countSDL_StartTextInput]);
+    ASSERT_EQ(0, actions.count[countSDL_StopTextInput]);
+    application.enableTextInput(false);
+}
+
+TEST(ApplicationTest, ApplicationStart2Stop2Text)
+{
+    MocksSDLActions     actions;
+    MockSDL             mockActivate(actions);
+    ThorsAnvil::UI::Application             application;
+
+    auto action = [&application]()
+    {
+        application.enableTextInput(true);
+        application.enableTextInput(true);
+        application.enableTextInput(false);
+        application.enableTextInput(false);
+    };
+
+    EXPECT_NO_THROW(
+        action();
+    );
+
+    ASSERT_EQ(1, actions.count[countSDL_StartTextInput]);
+    ASSERT_EQ(1, actions.count[countSDL_StopTextInput]);
 }
 
